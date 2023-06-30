24750daab0463b885a50766210c65c89d92feeff
noe <jesus.echeverria017@gmail.com>
2023-06-30 13:31:52 -0600

    Diseñando tarjeta para projectos modulo ecperience

--- src/App.js
+++ src/App.js
@@ -55,6 +55,28 @@ function App() {
           <Card_CV />
         </section>   
         <About_Me/>
+
+{/* Tarjetas de proyectos */}
+        <div className='flex flex-col items-center w-full h-auto  p-5 rounded-lg shadow-lg bg-[#f2f7ff]'>
+       <div className='flex pb-5'>
+         <img className="h-auto w-full rounded-lg hover:scale-[0.97] ease-in duration-300" src="/JesusEcheverria.github.io/Images/Page_Developers_iti.jpg" alt="" />
+       </div>
+       <div className='flex flex-col pb-5'>
+         <h2 className='font-bold text-3xl md:text-3xl text-blue-700 text-center pb-5'>SCHOOL BOOK</h2>
+
+         <p className='text-justify font-normal text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam culpa accusamus officia quia exercitationem esse ratione qui, minima in perferendis error iusto quos sequi possimus non doloremque voluptatem, ipsa minus.</p>
+       </div>
+       <div className='flotar hover:scale-125 ease-in duration-300'>
+       <a href="https://github.com/EcheverriaJesus" target="_blank" rel="noopener noreferrer">
+                <svg className='w-9' viewBox="0 0 24 24">
+                    <path
+                        d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"
+                        fill="#000000" />
+                </svg>
+            </a>
+       </div>
+        </div>
+
       </main>
     </div>
   );
--- src/Components/Button_Icon.js
+++ src/Components/Button_Icon.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button_Icon(props) {
     return (
         <div className="hover:scale-150 ease-in duration-300">
-            <a href={ props.url} target="_blank" rel="noopener noreferrer">
+            <a href={ props.url } target="_blank" rel="noopener noreferrer">
                 <svg className='w-9' viewBox="0 0 24 24">
                     <path
                         d={ props.path}
