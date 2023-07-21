bdd133ca866951398df512a164786f5e98d639d8
noe <jesus.echeverria017@gmail.com>
2023-07-20 19:59:11 -0600

    Trabajando en el diseño de la informacion de los proyectos

--- src/Views/Proyect3D.js
+++ src/Views/Proyect3D.js
@@ -3,11 +3,13 @@ import { Carousel } from 'flowbite-react';
 
 export default function Proyect3D() {
   return (
-    <div className='flex h-screen flex-col w-full md:p-24 md:flex-row prueba'>
-      <div className='bg-red-200'>
-        <h3>Trabajando en estas rutas para los proyectos</h3>
+    <div className='flex h-screen flex-col w-full md:p-24 md:flex-row'>
+      <div className='flex flex-col justify-center w-full gap-4 pr-10 md:w-1/2'>
+      <h2 className='font-bold text-3xl md:text-5xl text-w'>Objectos 3D</h2>
+        <h4 className='text-base text-justify font-semibold'>Aprendí a utilizar blender por pasatiempo. Me gusta la tecnologia 3D.</h4>
+        <p className='font-normal text-sm text-justify text-slate-600'>Gracias a lo autodidacta he adquirido experiencia para lograr modelar Objectos sencillos, ademas tengo los conocimientos básicos de renderizar texturas en mis objectos asi como la luz y movimiento de camaras que ofrece blender.</p>
       </div>
-      <div className='flex w-full h-1/2 md:h-auto'>
+      <div className='flex w-full h-1/2 md:h-full'>
         <Carousel>
           <img
             alt="..."
