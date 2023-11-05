c200884305d57e5c90048d64ccbe3fbf716aa0af
noe <jesus.echeverria017@gmail.com>
2023-11-04 19:29:51 -0600

    Proyectos: Trabajando en el modulo de proyctos

--- src/Views/OpcionesMenu/Proyectos.js
+++ src/Views/OpcionesMenu/Proyectos.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 export default function Proyectos() {
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
+    <div className='h-auto flex flex-col justify-center items-center'>
+    
       <section className='flex flex-col justify-center items-center w-full'>
         <h3 className='text-gray-900 text-3xl pb-2 lg:text-4xl font-semibold dark:text-gray-200 font-roboto'>Algunos de mis Proyectos</h3>
         <p className='text-gray-600 text-md lg:text-lg dark:text-gray-400 font-roboto'>Interfaces atractivas,  intuitivas y con un buen flujo de UX</p>
+        
       </section>
        <section className='flex relative justify-center items-center w-full h-full'>
        
-        <article className='absolute top-10 left-0 w-72 h-56 rounded-md p-2 bg-white flotar z-10'>
+        {/* <article className='absolute top-10 left-0 w-72 h-56 rounded-md p-2 bg-white flotar z-10'>
         <img className="w-full rounded-md" src="/JesusEcheverria.github.io/Images/SchoolBook/SCHOOLBOOK2.png" alt="" />
         <div className='flex flex-col justify-center items-center space-y-2'>
           <h5 className='pt-3 text-base lg:text-md font-semibold font-roboto'>SchoolBook (Sistema Escolar)</h5>
@@ -61,7 +63,7 @@ export default function Proyectos() {
         <div className='flex justify-center pt-2'>
         <svg className='w-5 hover:scale-125 duration-200 hover:cursor-pointer' viewBox="0 0 15 15"><path d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z"></path></svg>
         </div>
-        </article>
+        </article> */}
           {/* <img className="absolute bottom-14 left-80 w-12 girarLogo" src="/JesusEcheverria.github.io/Images/LogoHerramientas/Laravel.png" alt="" /> */}
           {/* <img className="absolute -bottom-8 right-6 w-14 flotarIcono" src="/JesusEcheverria.github.io/Images/LogoHerramientas/Java.png" alt="" /> */}
           {/* <span className="w-12 h-12 bg-transparent rounded-full absolute border  animate-pulse border-gray-300 dark:border-gray-700"></span>
@@ -70,6 +72,73 @@ export default function Proyectos() {
           <span className="w-96 h-96 bg-transparent rounded-full absolute border border-gray-200 dark:border-gray-800"></span>
           <span className="w-[550px] h-[550px] bg-transparent rounded-full absolute border border-gray-100 dark:border-gray-900"></span> */}
           {/* <span className="w-16 h-16 bg-[#180828] rounded-full absolute dark:bg-[#A2E9C1] animate-ping"></span> */}
+      </section>
+      <section className='grid grid-cols-1 w-full py-14 gap-16 md:grid-cols-2 md:gap-10 lg:grid-cols-3 2xl:grid-cols-2'>
+        <article className='w-full h-auto space-y-7'>
+          <div className='text-center w-full'>
+            <h4 className='text-xl lg:text-2xl font-semibold font-roboto dark:text-white'>SchoolBook 📒</h4>
+            <p className='font-roboto text-sm text-gray-700 dark:text-gray-400'>
+              Encontraras botones y cards para poder utilizar en tus proyectos.
+            </p>
+          </div>
+          <div>
+            <a href='https://uiverse.io/profile/EcheverriaJesus' target='_blank' rel='noopener noreferrer'>
+              <img className="rounded-lg hover:scale-95 duration-200" src="/JesusEcheverria.github.io/Images/SchoolBook/SCHOOLBOOK2.png" alt="" />
+            </a>
+          </div>
+        </article>
+        <article className='w-full h-auto space-y-7'>
+          <div className='text-center w-full'>
+            <h4 className='text-xl lg:text-2xl font-semibold font-roboto dark:text-white'>Cinteolt 🥗</h4>
+            <p className='font-roboto text-sm text-gray-700 dark:text-gray-400'>
+              Te compartó los paso para traducir Laravel Breeze y Jetstream a español.
+            </p>
+          </div>
+          <div>
+            <a href='https://dev.to/echeverriajesus/breeze-y-jetstream-en-espanol-4onn' target='_blank' rel='noopener noreferrer'>
+              <img className="rounded-lg hover:scale-95 duration-200" src="/JesusEcheverria.github.io/Images/Cinteolt/Cinteotl3.png" alt="" />
+            </a>
+          </div>
+        </article>
+        <article className='w-full h-auto space-y-7'>
+          <div className='text-center w-full'>
+            <h4 className='text-xl lg:text-2xl font-semibold font-roboto dark:text-white'>Modelos 3D 🎲</h4>
+            <p className='font-roboto text-sm text-gray-700 dark:text-gray-400'>
+              Te compartó los paso para traducir Laravel Breeze y Jetstream a español.
+            </p>
+          </div>
+          <div>
+            <a href='https://dev.to/echeverriajesus/breeze-y-jetstream-en-espanol-4onn' target='_blank' rel='noopener noreferrer'>
+              <img className="rounded-lg hover:scale-95 duration-200" src="/JesusEcheverria.github.io/Images/3D/Barril_3D.jpg" alt="" />
+            </a>
+          </div>
+        </article>
+        <article className='w-full h-auto space-y-7'>
+          <div className='text-center w-full'>
+            <h4 className='text-xl lg:text-2xl font-semibold font-roboto dark:text-white'>Developers 👨‍💻</h4>
+            <p className='font-roboto text-sm text-gray-700 dark:text-gray-400'>
+              Encontraras botones y cards para poder utilizar en tus proyectos.
+            </p>
+          </div>
+          <div>
+            <a href='https://uiverse.io/profile/EcheverriaJesus' target='_blank' rel='noopener noreferrer'>
+              <img className="rounded-lg hover:scale-95 duration-200" src="/JesusEcheverria.github.io/Images/Developers/Developers3.jpg" alt="" />
+            </a>
+          </div>
+        </article>
+        <article className='w-full h-auto space-y-7'>
+          <div className='text-center w-full'>
+            <h4 className='text-xl lg:text-2xl font-semibold font-roboto dark:text-white'>Pos System 🛒</h4>
+            <p className='font-roboto text-sm text-gray-700 dark:text-gray-400'>
+              Te compartó los paso para traducir Laravel Breeze y Jetstream a español.
+            </p>
+          </div>
+          <div>
+            <a href='https://dev.to/echeverriajesus/breeze-y-jetstream-en-espanol-4onn' target='_blank' rel='noopener noreferrer'>
+              <img className="rounded-lg hover:scale-95 duration-200" src="/JesusEcheverria.github.io/Images/PosSystem/PosSystem6.png" alt="" />
+            </a>
+          </div>
+        </article>
       </section>
     </div>
   )
